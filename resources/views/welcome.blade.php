b9dcc29a4ea7ec8562489a42ab638c564221ef2b
vicSystems <victorasuquob@gmail.com>
2023-03-21 14:26:53 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from wpthemebooster.com/demo/themeforest/html/oculus/index-5.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 21 Mar 2023 12:25:32 GMT -->
+<head>
+        <!-- Meta Tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <meta name="description" content="Portfolio HTML5 Template">
+        <meta name="author" content="">
+
+        <!-- Page Title -->
+        <title>Cyntia & Paul | Wedding</title>
+
+        <!-- Favicon and touch Icons -->
+        <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+        <link href="images/apple-touch-icon.html" rel="apple-touch-icon">
+        <link href="images/apple-touch-icon-72x72.html" rel="apple-touch-icon" sizes="72x72">
+        <link href="images/apple-touch-icon-114x114.html" rel="apple-touch-icon" sizes="114x114">
+        <link href="images/apple-touch-icon-144x144.html" rel="apple-touch-icon" sizes="144x144">
+
+		<!-- Lead Style -->
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+	<!-- Preloader -->
+	<div class="oculoader">
+		<div class="loader"></div>
+		<div class="top-side"></div>
+		<div class="bottom-side"></div>
+	</div>
+	<!--PreLoader End-->
+
+    <body class="body_full dark_mode">
+
+		<div class="wrapper">
+			<!-- Start Header -->
+	        <header id="header" class="header_two">
+	            <div class="nav-bar">
+					<div class="navigation">
+						<div class="nav-more-info">
+							<div class="element">
+								<div id="mobile-toggle" class="mobile-btn">
+									<span class="toggle_line"></span>
+									<span class="toggle_line"></span>
+									<span class="toggle_line"></span>
+								</div>
+
+
+							</div>
+						</div>
+						<div class="logo">
+							<a href="index.html"><img src="images/logo-white.png" alt="logo"></a>
+						</div>
+						<div class="nav_contact">
+							<div class="element">
+								<h6 class="email">res@cynthiaandpaul.us</h6>
+								<h6 class="phone">+23480337835670</h6>
+							</div>
+						</div>
+					</div>
+	            </div>
+
+	            <div class="overlaynav">
+	                <a href="#" class="overlaynavclose">&times;</a>
+	                <nav id="overlaymenu">
+	                	<ul class="main-menu">
+	                		<li class="menu-item active"><a href="index.html">Home</a>
+
+							</li>
+							<li class="menu-item"><a href="#">Gallery</a>
+
+							</li>
+
+							<li class="menu-item"><a href="#">Contacts</a>
+
+							</li>
+
+						</ul>
+	                </nav>
+	            </div>
+	        </header>
+
+			<div class="inner_body">
+				<div class="slide_3">
+					<div class="swiper-container">
+						<div class="swiper-wrapper">
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/slider/bg_3.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h4 class="slider-tag">15th, APRIL, 2023</h4>
+											<h1 class="slide_heading"><span>Cynthia & Paul</span></h1>
+											<div class="button_group">
+												<a href="portfolio-1.html" class="button effect_1 fade_black_button"><span>Explore Gallery</span></a>
+											</div>
+										</div>
+										<a class="popup fancyswiper" href="images/slider/bg_3.jpg" data-fancybox="images">
+											<img src="images/icon-img/g-icon2.png" alt="icon">
+										</a>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/slider/bg_4.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h4 class="slider-tag">Travelling</h4>
+											<h1 class="slide_heading"><span>Beautiful Nature</span></h1>
+											<div class="button_group">
+												<a href="portfolio-2.html" class="button effect_2 fade_black_button"><span>Explore Gallery</span></a>
+											</div>
+										</div>
+										<a class="popup fancyswiper" href="images/slider/bg_4.jpg" data-fancybox="images">
+											<img src="images/icon-img/g-icon2.png" alt="icon">
+										</a>
+									</div>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/slider/bg_2.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h4 class="slider-tag">Travelling</h4>
+											<h1 class="slide_heading"><span>Incredible Iceland</span></h1>
+											<div class="button_group">
+												<a href="portfolio-3.html" class="button effect_1 fade_black_button"><span>Explore Gallery</span></a>
+											</div>
+										</div>
+										<a class="popup fancyswiper" href="images/slider/bg_2.jpg" data-fancybox="images">
+											<img src="images/icon-img/g-icon2.png" alt="icon">
+										</a>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/slider/bg_3.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h4 class="slider-tag">Travelling</h4>
+											<h1 class="slide_heading"><span>Greatest Adventure</span></h1>
+											<div class="button_group">
+												<a href="portfolio-4.html" class="button effect_3 fade_black_button"><span>Explore Gallery</span></a>
+											</div>
+										</div>
+										<a class="popup fancyswiper" href="images/slider/bg_3.jpg" data-fancybox="images">
+											<img src="images/icon-img/g-icon2.png" alt="icon">
+										</a>
+									</div>
+								</div>
+						  	</div>
+						</div>
+					</div>
+					<div class="swiper-controller">
+						<!-- Add Pagination -->
+						<div class="swiper-pagination"></div>
+
+						<!-- If we need navigation buttons -->
+						<div class="swiper-navigation">
+							<div class="swiper-button-prev"></div>
+							<div class="swiper-button-next"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
+			<footer class="footer">
+				<div class="footer_inner">
+					<div class="copyright">
+						<p> &copy;vicSystems Technologies Ltd. 2023. All rights reserved</p>
+					</div>
+					<div class="social-icon">
+						<ul>
+							<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</div>
+
+        <!-- All JavaScript Files
+        ================================================== -->
+		<script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <script src="js/jquery.fancybox.min.js"></script>
+		<script src="js/swiper.min.js"></script>
+    	<script src="js/magnetCurs.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/menu.js"></script>
+		<script src="js/custom.js"></script>
+		<script>
+			magnetCurs.init({
+				click: 300,
+				spacing: 10,
+				pointer: true,
+				shockable: 1
+			});
+		</script>
+	</body>
+
+<!-- Mirrored from wpthemebooster.com/demo/themeforest/html/oculus/index-5.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 21 Mar 2023 12:26:29 GMT -->
+</html>

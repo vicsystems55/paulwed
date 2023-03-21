74f509c1edabecd0377760afe308b5a2d4ce6f86
vicSystems <victorasuquob@gmail.com>
2023-03-21 15:19:24 +0100

    home and gallery

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -7,7 +7,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
-        <meta name="description" content="Portfolio HTML5 Template">
+        <meta name="description" content="Cynthia weds Paul">
         <meta name="author" content="">
 
         <!-- Page Title -->
